e58b2ea056173e43ea83758a694b31bb607926f1
fazrienoer7-sys <fazrienoer7@gmail.com>
2026-06-02 15:11:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -347,3 +347,30 @@ if(toggle && nav){
 console.log(
   "Unity~LegiOn` Website Ready!"
 );
+
+function revealSections(){
+
+  const reveals =
+  document.querySelectorAll(".reveal");
+
+  for(let i = 0; i < reveals.length; i++){
+
+    const windowHeight =
+    window.innerHeight;
+
+    const revealTop =
+    reveals[i]
+    .getBoundingClientRect().top;
+
+    const revealPoint = 120;
+
+    if(revealTop < windowHeight - revealPoint){
+
+      reveals[i]
+      .classList.add("active");
+
+    }
+
+  }
+
+}
